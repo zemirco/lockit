b88e2ca7236df4cf3b0b0f212712e65ad2d8efbe
Mirco <mirco.zeiss@gmail.com>
2013-10-20 18:28:50 +0200

    auto create couchdb views

--- index.js
+++ index.js
@@ -4,9 +4,56 @@ var login = require('lockit-login');
 var forgotPassword = require('lockit-forgot-password');
 var deleteAccount = require('lockit-delete-account');
 
+// create couchdb views
+function createCouchDBViews(config) {
+
+  // create db connection
+  var db = require('nano')({
+    url: config.dbUrl,
+    request_defaults: config.request_defaults
+  });
+
+  // list of couchdb views
+  var views = {
+    _id: '_design/users',
+    views: {
+      username: {
+        map: function(doc) {
+          emit(doc.username, doc);
+        }
+      },
+      email: {
+        map: function(doc) {
+          emit(doc.email, doc);
+        }
+      },
+      signupToken: {
+        map: function(doc) {
+          emit(doc.signupToken, doc);
+        }
+      },
+      pwdResetToken: {
+        map: function(doc) {
+          emit(doc.pwdResetToken, doc);
+        }
+      }
+    }
+  };
+
+  // save views to db
+  db.insert(views, function(err, body) {
+    if (err) console.log(err);
+    console.log('done');
+  });
+}
+
 // just a wrapper around the single modules
 module.exports = function(app, config) {
 
+  // create views if we are working with CouchDB
+  if (config.db === 'couchdb') createCouchDBViews(config);
+
+  // load all required modules
   signup(app, config);
   login(app, config);
   forgotPassword(app, config);
