eed6cc0184909d52ee37db3308ae25e16dc19340
zeMirco <mirco.zeiss@gmail.com>
2013-12-28 15:37:59 +0100

    use index file to separate from others

--- example/app.js
+++ example/app.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
+var routes = require('./routes/index');
 var user = require('./routes/user');
 var profile = require('./routes/profile');
 var http = require('http');
