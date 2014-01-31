febae6e8ba48a53f6915b804bba0d2895638fc8f
zeMirco <mirco.zeiss@gmail.com>
2014-01-31 22:24:08 +0100

    restructure test app to support config as arg

--- test/app.js
+++ test/app.js
@@ -10,43 +10,58 @@ var http = require('http');
 var path = require('path');
 
 var lockit = require('../index.js');
-var config = require('./config.js');
 
-var app = express();
+function start(config) {
+
+//  config = config || require('./config.js');
+  
+  var app = express();
 
 // set basedir so views can properly extend layout.jade
-app.locals.basedir = __dirname + '/views';
+  app.locals.basedir = __dirname + '/views';
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+  app.set('port', process.env.PORT || config.port ||  3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
 
 // activate sessions
-app.use(express.cookieParser('your secret here'));
-app.use(express.cookieSession());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieSession());
 
-app.use(app.router);
+  app.use(app.router);
 
 // load lockit routes
-lockit(app, config);
+  lockit(app, config);
 
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  app.get('/', routes.index);
+  app.get('/users', user.list);
+
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+  
+  return app;
+  
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-// export app for testing purpose
-module.exports = app;
+// export app for testing
+if(require.main === module){
+  // called directly
+  start();
+} else {
+  // required as a module -> from test file
+  module.exports = function(config) {
+    return start(config);
+  };
+}
