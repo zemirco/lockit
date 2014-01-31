94250bae1b565092cdcbe6ca7c15b1daf895950a
zeMirco <mirco.zeiss@gmail.com>
2014-01-31 20:23:52 +0100

    add simple examples

---
+++ examples/couchdb/config.js
@@ -0,0 +1,8 @@
+
+// create a database in CouchDB first (i.e. 'lockit')
+
+// settings for local CouchDB
+exports.db = 'http://127.0.0.1:5984/lockit';
+
+// settings for CouchDB in cloud (i.e. Cloudant)
+// exports.db = 'https://<user>:<password>@<username>.cloudant.com/lockit';
---
+++ examples/mongodb/config.js
@@ -0,0 +1,8 @@
+
+// settings for local MongoDB
+exports.db = 'mongodb://localhost:27017/lockit';
+exports.dbCollection = 'users';
+
+// settings for MongoDB in cloud (i.e. MongoLab or MongoHQ)
+//exports.db = 'mongodb://<user>:<password>@<id>.mongolab.com:<port>/lockit';
+//exports.dbCollection = 'users';
