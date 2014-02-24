fd0e1693e1707e635a85be5e6d5e3cd040fe60ed
zeMirco <mirco.zeiss@gmail.com>
2014-02-24 19:25:34 +0100

    use new lockit constructor and remove bodyParser

--- test/app.js
+++ test/app.js
@@ -9,37 +9,36 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
-var lockit = require('../index.js');
+var Lockit = require('../index.js');
 
 function start(config) {
-
-//  config = config || require('./config.js');
   
   var app = express();
 
-// set basedir so views can properly extend layout.jade
+  // set basedir so views can properly extend layout.jade
   app.locals.basedir = __dirname + '/views';
 
-// all environments
+  // all environments
   app.set('port', process.env.PORT || config.port ||  3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.bodyParser());
+  app.use(express.urlencoded());
+  app.use(express.json());
   app.use(express.methodOverride());
 
-// activate sessions
+  // activate sessions
   app.use(express.cookieParser('your secret here'));
   app.use(express.cookieSession());
 
   app.use(app.router);
 
-// load lockit routes
-  lockit(app, config);
+  // load lockit routes
+  var lockit = new Lockit(app, config);
 
   app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+  // development only
   if ('development' == app.get('env')) {
     app.use(express.errorHandler());
   }
@@ -47,9 +46,7 @@ function start(config) {
   app.get('/', routes.index);
   app.get('/users', user.list);
 
-  http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-  });
+  http.createServer(app).listen(app.get('port'));
   
   return app;
   
