261c93a2ff3bd5ac8635ea81d9d3c3e1eef4317f
zeMirco <mirco.zeiss@gmail.com>
2014-04-19 12:08:43 +0200

    use express 4.x in test app

--- test/app.js
+++ test/app.js
@@ -8,6 +8,11 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var favicon = require('static-favicon');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
 
 var Lockit = require('../index.js');
 
@@ -15,32 +20,28 @@ function start(config) {
 
   var app = express();
 
-  // set basedir so views can properly extend layout.jade
-  app.locals.basedir = __dirname + '/views';
+  config = config || require('./config.js');
 
   // all environments
   app.set('port', process.env.PORT || config.port ||  3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.urlencoded());
-  app.use(express.json());
-  app.use(express.methodOverride());
-
-  // activate sessions
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.cookieSession());
-
-  app.use(app.router);
+  app.use(favicon());
+  app.use(bodyParser.urlencoded());
+  app.use(bodyParser.json());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(cookieParser());
+  app.use(cookieSession({
+    secret: 'this is my super secret string'
+  }));
 
   // load lockit routes
-  var lockit = new Lockit(app, config);
-
-  app.use(express.static(path.join(__dirname, 'public')));
+  var lockit = new Lockit(config);
+  app.use(lockit.router);
 
   // development only
   if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+    app.use(errorHandler());
   }
 
   app.get('/', routes.index);
