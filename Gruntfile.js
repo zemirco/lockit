fcc3a9f3c5a659c4c0b140b57548ea70a40e906a
zeMirco <mirco.zeiss@gmail.com>
2014-05-28 22:12:32 +0200

    add events tests to grunt mocha task

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
         options: {
           reporter: 'spec'
         },
-        src: ['test/test.js']
+        src: ['test/test.js', 'test/events.test.js']
       }
     }
   });
