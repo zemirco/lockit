390aa3f97f2669468967fac67bd8aa82037dd471
Mirco Zeiss <mirco.zeiss@gmail.com>
2015-06-29 18:52:49 +0200

    update dependencies

--- test/events.test.js
+++ test/events.test.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var express = require('express');
 var http = require('http');
@@ -12,7 +13,8 @@ var config = require('./config.js');
 var lockit = new Lockit(config);
 
 var app = express();
-app.set('views', __dirname + '/views');
+// app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(favicon());
 app.use(bodyParser.urlencoded());
@@ -31,13 +33,13 @@ var request = require('supertest');
 describe('events', function() {
 
   before(function(done) {
-    lockit.adapter.save('event', 'event@email.com', 'password', function(err, user) {
-      if (err) console.log(err);
-      lockit.adapter.find('name', 'event', function(err, user) {
-        if (err) console.log(err);
+    lockit.adapter.save('event', 'event@email.com', 'password', function(err) {
+      if (err) {console.log(err); }
+      lockit.adapter.find('name', 'event', function(e, user) {
+        if (e) {console.log(e); }
         user.emailVerified = true;
-        lockit.adapter.update(user, function(err, user) {
-          if (err) console.log(err);
+        lockit.adapter.update(user, function(error) {
+          if (err) {console.log(error); }
           done();
         });
       });
@@ -47,18 +49,18 @@ describe('events', function() {
   var agent = request.agent('http://localhost:7000');
 
   it('should emit "signup" event', function(done) {
-    lockit.on('signup', function(user, res) {
+    lockit.on('signup', function(user) {
       user.name.should.equal('john');
       done();
     });
     agent
       .post('/signup')
       .send({name: 'john', email: 'john@email.com', password: 'password'})
-      .end(function(err, res) {
-        if (err) console.log(err);
+      .end(function(err) {
+        if (err) {console.log(err); }
         // get token from db
-        lockit.adapter.find('name', 'john', function(err, user) {
-          if (err) console.log(err);
+        lockit.adapter.find('name', 'john', function(error, user) {
+          if (err) {console.log(error); }
           // visit /signup/:token
           agent
             .get('/signup/' + user.signupToken)
@@ -75,54 +77,54 @@ describe('events', function() {
     });
     agent
       .post('/login')
-      .send({login:'event', password:'password'})
-      .end(function(err, res) {
-        if (err) console.log(err);
+      .send({login: 'event', password: 'password'})
+      .end(function(err) {
+        if (err) {console.log(err); }
       });
   });
 
   it('should emit "logout" event', function(done) {
-    lockit.on('logout', function(user, res) {
+    lockit.on('logout', function(user) {
       user.name.should.equal('event');
       done();
     });
     agent
       .get('/logout')
-      .end(function(err, res) {
-        if (err) console.log(err);
+      .end(function(err) {
+        if (err) {console.log(err); }
       });
   });
 
   it('should emit "forgot::sent" event', function(done) {
     lockit.removeAllListeners();
-    lockit.on('forgot::sent', function(user, res) {
+    lockit.on('forgot::sent', function(user) {
       user.name.should.equal('event');
       done();
     });
     agent
       .post('/forgot-password')
-      .send({email:'event@email.com'})
-      .end(function(err, res) {
-        if (err) console.log(err);
+      .send({email: 'event@email.com'})
+      .end(function(err) {
+        if (err) {console.log(err); }
       });
   });
 
   it('should emit "forgot::success" event', function(done) {
     lockit.removeAllListeners();
 
-    lockit.on('forgot::success', function(user, res) {
+    lockit.on('forgot::success', function(user) {
       user.name.should.equal('event');
       done();
     });
 
     lockit.adapter.find('name', 'event', function(err, user) {
-      if (err) console.log(err);
-      token = user.pwdResetToken;
+      if (err) {console.log(err); }
+      var token = user.pwdResetToken;
       agent
         .post('/forgot-password/' + token)
         .send({password: 'new-password'})
-        .end(function(err, res) {
-          if (err) console.log(err);
+        .end(function(error) {
+          if (err) {console.log(error); }
         });
     });
   });
@@ -130,23 +132,23 @@ describe('events', function() {
   it('should emit "delete" event', function(done) {
     // remove 'login' event listener
     lockit.removeAllListeners();
-    lockit.on('delete', function(user, res) {
+    lockit.on('delete', function(user) {
       user.name.should.equal('event');
       done();
     });
     // login first because last test logged agent out
     agent
       .post('/login')
-      .send({login:'event', password:'new-password'})
-      .end(function(err, res) {
-        if (err) console.log(err);
+      .send({login: 'event', password: 'new-password'})
+      .end(function(err) {
+        if (err) {console.log(err); }
         // post delete account
         process.nextTick(function() {
           agent
             .post('/delete-account')
             .send({name: 'event', password: 'new-password', phrase: 'please delete my account forever'})
-            .end(function(err, res) {
-              if (err) console.log(err);
+            .end(function(error) {
+              if (err) {console.log(error); }
             });
         });
       });
